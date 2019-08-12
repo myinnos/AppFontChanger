951df0537bed51bc7343131d5fd98c78186acead
Prabhakar Thota <prabhakar_thota@live.com>
2019-08-12 14:40:40 +0530

    * updated to androidX

--- app/src/main/java/in/myinnos/customfont/MainActivity.java
+++ app/src/main/java/in/myinnos/customfont/MainActivity.java
@@ -1,6 +1,6 @@
 package in.myinnos.customfont;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
